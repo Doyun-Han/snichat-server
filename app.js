c0482f29bafdf53e409dfff6126551098c314a1b
han <dy__z@nate.com>
2022-09-22 16:47:08 +0900

    make GET chatlists, POST msgs

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-error';
+import morgan from "morgan";
+import cors from "cors";
+import helmet from 'helmet';
+import listsRouter from './router/lists.js'
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.use('/chat-lists', listsRouter)
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+})
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.sendStatus(500);
+})
+
+app.listen(8080);
